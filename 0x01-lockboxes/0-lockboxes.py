a3e0ba48f0d1ca7bcb9edd30326fb4c07360b325
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-04-30 02:49:08 -0700

    code for 0x01. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+LockBoxes
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened
+    
+    Args:
+        boxes: list of lists where each inner list represents the keys in a box
+
+        Returns: 
+            True if all boxes can be opened and false if not
+    """
+
+    n = len(boxes)
+    keys = {0}
+    visited = {0}
+
+    while keys:
+        nxt_key = keys.pop()
+        for key in boxes[nxt_key]:
+            if key not in visited and 0 < key < n:
+                keys.add(key)
+                visited.add(key)
+
+    return (len(visited) == n)
