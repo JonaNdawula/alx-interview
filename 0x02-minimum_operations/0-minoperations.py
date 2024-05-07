6f4bdb7af6b8d42ca7a5fad5327cf47816da0498
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-07 03:15:33 -0700

    code for  0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+Contains the function minOperations
+that calculates the fewest number of
+operations needed to result in exactly n H
+chararacters in the file
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file
+
+    Parameters:
+    n : The target number of H characters
+
+    Returns:
+    minimum number of operations
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n = n // divisor
+        divisor += 1
+
+    return operations
