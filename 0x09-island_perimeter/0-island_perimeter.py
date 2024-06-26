2d7e9972e6a28fcfffbb5de9e7d013782cbc8c3b
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-26 02:47:35 -0700

    code update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -24,7 +24,7 @@ def island_perimeter(grid):
                 if row > 0 and grid[row - 1][col] == 1:
                     edges -= 1
 
-                if row < rows and grid[row + 1][col] == 1:
+                if row < rows - 1 and grid[row + 1][col] == 1:
                     edges -= 1
 
                 if col > 0 and grid[row][col - 1] == 1:
